7c1fb430782b8495e922e281419dce16b767e79d
syafiqahlukman <ssyafiqahlukman@gmail.com>
2024-08-28 19:24:33 +0800

    python class 1

--- numbers.py
+++ numbers.py
@@ -1,2 +1,7 @@
 my_num = 5
 print(str((my_num)) + " is my favourite number")
+
+print(pow(4,6)) #4 power of 6
+print(max(4,6)) #print which one is largest number
+print(min(4,6)) #print which one is smallest number
+print(round(3.7)) #round to number whether up or down
