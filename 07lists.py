2665ca69df2d9b6615add508cf28f0e1ab1d135e
syafiqahlukman <ssyafiqahlukman@gmail.com>
2024-08-29 16:08:06 +0800

    python lesson2

---
+++ 07lists.py
@@ -0,0 +1,8 @@
+friends = ["Ali", "Muthu", "Ah Hock", "Aminah", "Manisah","Alice"] #store different values in 1 list. can be string, int, boolean, can mixed up
+print(friends[3]) #print only 1 value, index starts from 0
+print(friends[1:-2]) #print range of value. starting from index you choose on the left, and the right side index will not be included
+
+#if you would like to modify the value
+friends[-2] = "Roshini"
+print(friends)
+ 
